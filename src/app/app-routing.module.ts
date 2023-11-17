3f3195bf31a9b6d695d980b01afb2d64fc6562a3
PetraHajdu <hpegyetem96@gmail.com>
2023-11-17 15:48:35 +0100

    install bootstrap and generate component seat-selection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SeatSelectionComponent } from './seat-selection/seat-selection.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'seat-selection', component: SeatSelectionComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
